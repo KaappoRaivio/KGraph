617abf6a3c525d36e33bbedc0394ab97f54d4eac
Kaappo Raivio <kaappo.raivio@gmail.com>
2023-03-13 16:44:18 +0200

    feat: add colors to sidebar

--- src/content/sidebar/FunctionEntry.jsx
+++ src/content/sidebar/FunctionEntry.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import styles from "./FunctionEntry.module.css";
 import InlineInput from "../../InlineInput";
-const FunctionEntry = ({ name, rawInput, onChange, onRemoval, index }) => {
+const FunctionEntry = ({ name, color, rawInput, onChange, onRemoval, index }) => {
   return (
-    <li className={styles.listItem}>
+    <li className={styles.listItem} style={{ background: color }}>
       <div className={styles.titleBar}>
         <p>
           {index + 1}. {name}
